d0a2e92204c5ebc404afc393c690ef7375bd5e68
bayustrio7 <bayustrio7@gmail.com>
2022-06-19 18:26:20 +0700

    testing

--- pages/story/[slug].tsx
+++ pages/story/[slug].tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import CodeBlock from "../../components/Markdown/CodeBlog";
 
 const detail = () => {
-  return <div>detail</div>;
+  return (
+    <div>
+      <CodeBlock content={"```tsx ```"} />
+    </div>
+  );
 };
 
 export default detail;
